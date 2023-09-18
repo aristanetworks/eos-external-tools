bdf055f28715fb0574821533d0014db560a014f9
Manith <manith@arista.com>
2023-09-18 04:26:53 +0000

    srcconfig: Added srcconfig to support source bundles
    Created template for source bundle with srcconfig, for packages to refer
    using repoName.
    Added logic to expand url template of source rpms fro pacakge manifest data.
    Updated tests to utilize the source bundles instead of srpm urls.

--- srcconfig/srcconfig.go
+++ srcconfig/srcconfig.go
@@ -3,10 +3,215 @@
 
 package srcconfig
 
+import (
+	"bytes"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"strings"
+	"text/template"
+
+	"code.arista.io/eos/tools/eext/util"
+	"github.com/spf13/viper"
+	"gopkg.in/yaml.v2"
+)
+
 // SrcRepoParamsOverride spec
 // Override default specs for source repo bundle in package manifest
 type SrcRepoParamsOverride struct {
 	VersionOverride   string `yaml:"version"`
 	SrcSuffixOverride string `yaml:"src-suffix"`
 	SigSuffixOverride string `yaml:"sig-suffix"`
+}
+
+// SrcBundle spec
+// To generate the source url for srpm
+type SrcBundle struct {
+	URLFormat         string            `yaml:"url-format"`
+	DefaultSrcSuffix  string            `yaml:"default-src-suffix"`
+	DefaultSigSuffix  string            `yaml:"default-sig-suffix"`
+	VersionLabels     map[string]string `yaml:"version-labels"`
+	HasDetachedSig    bool              `yaml:"has-detached-sig"`
+	urlFormatTemplate *template.Template
+}
+
+// SrcConfig spec
+// Mapping for all source repo bundles
+type SrcConfig struct {
+	SrcBundle map[string]*SrcBundle `yaml:"source-bundle"`
+}
+
+// SrcParams spec
+// Updated source and signature url paths
+type SrcParams struct {
+	SrcURL       string
+	SignatureURL string
+}
+
+func (s *SrcBundle) getTranslatedVersion(versionOverride string, errPrefix util.ErrPrefix) (string, error) {
+	var version string
+	if versionOverride == "" {
+		version = "default"
+	} else {
+		version = versionOverride
+	}
+	translatedVersion, isVersionLabel := s.VersionLabels[version]
+	if !isVersionLabel {
+		translatedVersion = version
+	}
+
+	if translatedVersion == "default" {
+		return "", fmt.Errorf("%s No defaults specified for source-bundle, please specify a version override",
+			errPrefix)
+	}
+
+	return translatedVersion, nil
+}
+
+func (s *SrcBundle) getFormattedSigURL(srcFormattedURL, sigSuffixOverride string, onUncompressed bool) string {
+	var sigFormattedURL string
+	if s.HasDetachedSig {
+		var sigSuffix string
+		if sigSuffixOverride == "" {
+			sigSuffix = s.DefaultSigSuffix
+		} else {
+			sigSuffix = sigSuffixOverride
+		}
+
+		var sigURLWithoutSuffix string
+		if onUncompressed {
+			sigURLWithoutSuffix = strings.TrimSuffix(srcFormattedURL, filepath.Ext(srcFormattedURL))
+		} else {
+			sigURLWithoutSuffix = srcFormattedURL
+		}
+		sigFormattedURL = sigURLWithoutSuffix + sigSuffix
+	} else {
+		sigFormattedURL = ""
+	}
+	return sigFormattedURL
+}
+
+func (s *SrcBundle) getSrcRepoParams(
+	pkgName string,
+	srcOverrideParams SrcRepoParamsOverride,
+	onUncompressed bool,
+	errPrefix util.ErrPrefix) (
+	*SrcParams, error) {
+
+	translatedVersion, err := s.getTranslatedVersion(srcOverrideParams.VersionOverride, errPrefix)
+	if err != nil {
+		return nil, err
+	}
+
+	var srcSuffix string
+	if srcOverrideParams.SrcSuffixOverride == "" {
+		srcSuffix = s.DefaultSrcSuffix
+	} else {
+		srcSuffix = srcOverrideParams.SrcSuffixOverride
+	}
+	urlData := struct {
+		Host       string
+		PathPrefix string
+		PkgName    string
+		Version    string
+		Suffix     string
+	}{
+		Host:       viper.GetString("SrcRepoHost"),
+		PathPrefix: viper.GetString("SrcRepoPathPrefix"),
+		PkgName:    pkgName,
+		Version:    translatedVersion,
+		Suffix:     srcSuffix,
+	}
+
+	var urlBuf bytes.Buffer
+	if err := s.urlFormatTemplate.Execute(&urlBuf, urlData); err != nil {
+		return nil, fmt.Errorf("%sError executing template %s with data %v",
+			errPrefix, s.URLFormat, urlData)
+	}
+	srcFormattedURL := urlBuf.String()
+
+	sigFormattedURL := s.getFormattedSigURL(srcFormattedURL, srcOverrideParams.SigSuffixOverride, onUncompressed)
+
+	return &SrcParams{
+		SrcURL:       srcFormattedURL,
+		SignatureURL: sigFormattedURL,
+	}, nil
+}
+
+func getSrcParamsWithoutBundle(srcFullURL, sigFullURL string) *SrcParams {
+	return &SrcParams{
+		SrcURL:       srcFullURL,
+		SignatureURL: sigFullURL,
+	}
+}
+
+func GetSrcParams(
+	pkgName string,
+	srcFullURL string,
+	bundleName string,
+	sigFullURL string,
+	srcOverrideParams SrcRepoParamsOverride,
+	onUncompressed bool,
+	srcConfig *SrcConfig,
+	errPrefix util.ErrPrefix) (
+	*SrcParams, error) {
+
+	var srcParams *SrcParams
+	var srcParamsErr error
+	if bundleName != "" {
+		reqSrcBundle, ok := srcConfig.SrcBundle[bundleName]
+		if !ok {
+			return nil, fmt.Errorf("%sSource bundle %s not found in manifest", errPrefix, bundleName)
+		}
+		srcParams, srcParamsErr = reqSrcBundle.getSrcRepoParams(
+			pkgName,
+			srcOverrideParams,
+			onUncompressed,
+			errPrefix)
+	} else {
+		srcParams = getSrcParamsWithoutBundle(srcFullURL, sigFullURL)
+	}
+
+	if srcParamsErr != nil {
+		return nil, srcParamsErr
+	}
+	return srcParams, nil
+}
+
+func LoadSrcConfig() (*SrcConfig, error) {
+	cfgPath := viper.GetString("SrcConfigFile")
+	_, statErr := os.Stat(cfgPath)
+	if statErr != nil {
+		if os.IsNotExist(statErr) {
+			return nil, fmt.Errorf("srcconfig.LoadSrcConfig: %s doesn't exist",
+				cfgPath)
+		}
+		return nil, fmt.Errorf("srcconfig.LoadSrcConfig: os.Stat on %s returned %s",
+			cfgPath, statErr)
+	}
+
+	yamlContents, readErr := ioutil.ReadFile(cfgPath)
+	if readErr != nil {
+		return nil, fmt.Errorf("srcconfig.LoadSrcConfig: ioutil.ReadFile on %s returned %s",
+			cfgPath, readErr)
+	}
+
+	var config SrcConfig
+	if parseErr := yaml.UnmarshalStrict(yamlContents, &config); parseErr != nil {
+		return nil, fmt.Errorf("srcconfig.LoadSrcConfig: Error parsing yaml file %s: %s",
+			cfgPath, parseErr)
+	}
+
+	for bundleName, srcBundle := range config.SrcBundle {
+		templateName := "srcRepoBundle_" + bundleName
+		t, parseErr := template.New(templateName).Parse(srcBundle.URLFormat)
+		if parseErr != nil {
+			return nil, fmt.Errorf(
+				"srcconfig.LoadSrcConfig: Error parsing source %s for src repo-bundle %s",
+				srcBundle.URLFormat, bundleName)
+		}
+		srcBundle.urlFormatTemplate = t
+	}
+	return &config, nil
 }
---
+++ srcconfig/srcconfig_test.go
@@ -0,0 +1,150 @@
+// Copyright (c) 2022 Arista Networks, Inc.  All rights reserved.
+// Arista Networks, Inc. Confidential and Proprietary.
+
+package srcconfig
+
+import (
+	"testing"
+
+	"github.com/spf13/viper"
+	"github.com/stretchr/testify/require"
+)
+
+func TestRepoConfigSrpm(t *testing.T) {
+	viper.Set("SrcConfigFile", "testData/sample-srcconfig1.yaml")
+	viper.Set("SrcRepoHost", "http://foo.org")
+	viper.Set("SrcRepoPathPrefix", "foo")
+	defer viper.Reset()
+
+	t.Log("Testing Load")
+	srcConfig, loadErr := LoadSrcConfig()
+	require.NoError(t, loadErr)
+	require.NotNil(t, srcConfig)
+	require.Contains(t, srcConfig.SrcBundle, "bundle-srpm")
+	srpmConfig := srcConfig.SrcBundle["bundle-srpm"]
+	require.NotNil(t, srpmConfig)
+	require.NotNil(t, srpmConfig.urlFormatTemplate)
+	t.Log("Load test PASSED")
+
+	t.Log("Testing URL Format")
+	expectedURL := "http://foo.org/foo/pkg/pkg-1.0.src.rpm"
+	versions := []string{"1.0"}
+	for _, version := range versions {
+		srpmParams, paramsErr := GetSrcParams(
+			"pkg",         // Package Name
+			"",            // Full Source URL
+			"bundle-srpm", // Bundle Name
+			"",            // Signature URL
+			SrcRepoParamsOverride{
+				VersionOverride:   version,
+				SrcSuffixOverride: "",
+				SigSuffixOverride: "",
+			},
+			false,     // onUncompressed
+			srcConfig, // Source Config
+			"",        // Error Prefix
+		)
+		require.NoError(t, paramsErr)
+		require.NotNil(t, srpmParams)
+		require.Equal(t, expectedURL, srpmParams.SrcURL)
+	}
+	t.Log("URL template test PASSED")
+}
+
+type srcconfigTestVariant struct {
+	BundleName        string
+	SrcOverrideParams SrcRepoParamsOverride
+	ExpectedSrcURL    string
+	ExpectedSigURL    string
+	OnUncompressed    bool
+}
+
+func TestRepoConfigTarBall(t *testing.T) {
+	viper.Set("SrcConfigFile", "testData/sample-srcconfig2.yaml")
+	viper.Set("SrcRepoHost", "http://foo.org")
+	viper.Set("SrcRepoPathPrefix", "foo")
+	defer viper.Reset()
+
+	testCases := map[string]srcconfigTestVariant{
+		"defaultSignatureTest": srcconfigTestVariant{
+			BundleName: "bundle-tarball1",
+			SrcOverrideParams: SrcRepoParamsOverride{
+				VersionOverride:   "1.1",
+				SrcSuffixOverride: "",
+				SigSuffixOverride: "",
+			},
+			ExpectedSrcURL: "http://foo.org/foo/tarballs/pkg/1.1/pkg.tar.gz",
+			ExpectedSigURL: "http://foo.org/foo/tarballs/pkg/1.1/pkg.tar.gz.sig",
+			OnUncompressed: false,
+		},
+		"overridenSignatureTest": srcconfigTestVariant{
+			BundleName: "bundle-tarball2",
+			SrcOverrideParams: SrcRepoParamsOverride{
+				VersionOverride:   "2.2",
+				SrcSuffixOverride: ".tar.bz2",
+				SigSuffixOverride: ".asc",
+			},
+			ExpectedSrcURL: "http://foo.org/foo/tarballs/pkg/2.2/pkg.tar.bz2",
+			ExpectedSigURL: "http://foo.org/foo/tarballs/pkg/2.2/pkg.tar.asc",
+			OnUncompressed: true,
+		},
+	}
+
+	t.Log("Testing SrcConfig Load")
+	srcConfig, loadErr := LoadSrcConfig()
+	require.NoError(t, loadErr)
+	require.NotNil(t, srcConfig)
+	t.Log("SrcConfig Load test PASSED")
+
+	for testName, variant := range testCases {
+		t.Logf("%s: Check bundle present", testName)
+		require.Contains(t, srcConfig.SrcBundle, variant.BundleName)
+		bundle := srcConfig.SrcBundle[variant.BundleName]
+		require.NotNil(t, bundle)
+		require.NotNil(t, bundle.urlFormatTemplate)
+		t.Logf("%s: Bundle test PASSED", testName)
+
+		srcParams, paramsErr := GetSrcParams(
+			"pkg",                     // Package Name
+			"",                        // Source Full URL
+			variant.BundleName,        // Bundle Name
+			"",                        // Signature Full URL
+			variant.SrcOverrideParams, // Override params
+			variant.OnUncompressed,    // OnUncompressed
+			srcConfig,                 // Source Config
+			"",                        // Error Prefix
+		)
+
+		t.Logf("%s: URL template", testName)
+		require.NoError(t, paramsErr)
+		require.NotNil(t, srcParams)
+		require.Equal(t, variant.ExpectedSrcURL, srcParams.SrcURL)
+		t.Logf("%s: URL template PASSED", testName)
+
+		t.Logf("%s: Signature path", testName)
+		require.Equal(t, variant.ExpectedSigURL, srcParams.SignatureURL)
+		t.Logf("%s: Signature path PASSED", testName)
+	}
+
+	t.Log("Version assert Check")
+	versionTestName := "emptyVersionTest"
+	versionTestVariant := srcconfigTestVariant{
+		BundleName:        "bundle-tarball3",
+		SrcOverrideParams: SrcRepoParamsOverride{},
+		ExpectedSrcURL:    "",
+		ExpectedSigURL:    "",
+		OnUncompressed:    false,
+	}
+	versionTestExpError := "No defaults specified for source-bundle, please specify a version override"
+	_, paramsErr := GetSrcParams(
+		"pkg",                                // Package Name
+		"",                                   // Full Source URL
+		versionTestVariant.BundleName,        // Bundle Name
+		"",                                   // Signature Full URL
+		versionTestVariant.SrcOverrideParams, // Override Params
+		versionTestVariant.OnUncompressed,    // OnUncompressed
+		srcConfig,                            // Source Config
+		"")                                   // Error Prefix
+	require.ErrorContains(t, paramsErr, versionTestExpError)
+	t.Logf("%s: Version check PASSED", versionTestName)
+}
