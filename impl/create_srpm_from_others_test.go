b527b1362bc2d5410cb36bf900d0a2c191de5780
Maciej Kisielewski <mkisielewski@arista.com>
2024-12-03 11:56:35 +0100

    simplify the signature path code

--- impl/create_srpm_from_others_test.go
+++ impl/create_srpm_from_others_test.go
@@ -8,37 +8,39 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func testTarballSig(t *testing.T, folder string) {
-	curPath, _ := os.Getwd()
-	workingDir := filepath.Join(curPath, "testData/tarballSig", folder)
-	tarballPath := map[string]string{
-		"checkTarball": filepath.Join(workingDir, "linux.10.4.1.tar.gz"),
-		"matchTarball": filepath.Join(workingDir, "libpcap-1.10.4.tar.gz"),
+func TestIsSigfileApplicable(t *testing.T) {
+	testCases := []struct {
+		tarballPath   string
+		signaturePath string
+		out1          bool
+		out2          bool
+	}{
+		{"foo.tar.gz", "foo.tar.gz.sig", true, false},
+		{"foo.tar.gz", "foo.tar.sig", true, true},
+		{"foobar.tar.gz", "signature", false, false},
+		{"foo.tar.gz", "bar.tar.gz.sig", false, false},
 	}
-	tarballSigPath := filepath.Join(workingDir, "libpcap-1.10.4.tar.gz.sig")
-
-	switch folder {
-	case "checkTarball":
-		ok, _ := checkValidSignature(tarballPath[folder], tarballSigPath)
-		require.Equal(t, false, ok)
-	case "matchTarball":
-		intermediateTarball, err := matchTarballSignCmprsn(
-			tarballPath[folder],
-			tarballSigPath,
-			workingDir,
-			"TestmatchTarballSignature : ",
-		)
-		os.Remove(intermediateTarball)
-		require.Equal(t, nil, err)
+	for _, tc := range testCases {
+		res1, res2 := isSigfileApplicable(tc.tarballPath, tc.signaturePath)
+		if res1 != tc.out1 || res2 != tc.out2 {
+			t.Errorf("isSigfileApplicable for (%s, %s) -> (%t, %t); expected (%t, %t)",
+				tc.tarballPath, tc.signaturePath, res1, res2, tc.out1, tc.out2)
+		}
 	}
-}
-
-func TestCheckTarballSignature(t *testing.T) {
-	t.Log("Test tarball Signatue Check")
-	testTarballSig(t, "checkTarball")
 }
 
 func TestMatchTarballSignature(t *testing.T) {
-	t.Log("Test tarball Signatue Match")
-	testTarballSig(t, "matchTarball")
+	t.Log("Test tarball Signature Match")
+	curPath, _ := os.Getwd()
+	workingDir := filepath.Join(curPath, "testData/tarballSig/checkTarball")
+	tarballSigPath := filepath.Join(workingDir, "libpcap-1.10.4.tar.gz.sig")
+	tarballPath := filepath.Join(workingDir, "libpcap-1.10.4.tar.gz")
+	intermediateTarball, err := matchTarballSignCmprsn(
+		tarballPath,
+		tarballSigPath,
+		workingDir,
+		"TestmatchTarballSignature : ",
+	)
+	os.Remove(intermediateTarball)
+	require.NoError(t, err)
 }
